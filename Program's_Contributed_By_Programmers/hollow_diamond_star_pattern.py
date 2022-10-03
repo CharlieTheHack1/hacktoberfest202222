888c3f8db1e71658ab043ebf819092622efd54cc
Zuhaib Khan <50094694+adnankhanmgdp@users.noreply.github.com>
2022-10-03 20:25:01 +0530

    Updated coloured_hollow_diamond_stars_pattern

--- Program's_Contributed_By_Programmers/hollow_diamond_star_pattern.py
+++ Program's_Contributed_By_Programmers/hollow_diamond_star_pattern.py
@@ -1,16 +1,23 @@
 n = int(input('Enter n:'))
+from colorama import Fore, Style
+
+l = [Fore.BLUE, Fore.GREEN, Fore.RED, Fore.WHITE, Fore.MAGENTA, Fore.YELLOW]
 
 def upper():
     global n;
+    c = 0
     for i in range(n-1):
-        print("* "*(n-i),"  "*i,sep="",end="")
-        print("  "*i,"* "*(n-i),sep="")
+        print(l[c],"* "*(n-i),"  "*i,sep="",end="")
+        print("  "*i,"* "*(n-i),Style.RESET_ALL,sep="")
+        c=[0,c+1][(c!=5)]
         
 def lower():
     global n
+    c = 0
     for i in range(n):
-        print("* "*(i+1),"  "*(n-i-1),sep="",end="")
-        print("  "*(n-i-1),"* "*(i+1),sep="")
+        print(l[c],"* "*(i+1),"  "*(n-i-1),sep="",end="")
+        print("  "*(n-i-1),"* "*(i+1),Style.RESET_ALL,sep="")
+        c=[0,c+1][(c!=5)]
 
 upper()
 lower()
