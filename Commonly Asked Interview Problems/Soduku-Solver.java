1f6f5c97364bfa0087dcb0dc9c2caf9a94781f26
Aarav Shukla <aarav8090shukla@gmail.com>
2022-10-07 09:43:31 +0530

    This is the leetcode solution of Soduku Solver .
    Soduku Solver is the one of the most asked questions in interview.

---
+++ Commonly Asked Interview Problems/Soduku-Solver.java
@@ -0,0 +1,83 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+        
+    }
+     static boolean solve(char[][] board) {
+        int n = board.length;
+        int row = -1;
+        int col = -1;
+
+        boolean emptyLeft = true;
+
+        // this is how we are replacing the r,c from arguments
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == '.') {
+                    row = i;
+                    col = j;
+                    emptyLeft = false;
+                    break;
+                }
+            }
+            // if you found some empty element in row, then break
+            if (emptyLeft == false) {
+                break;
+            }
+        }
+
+        if (emptyLeft == true) {
+            return true;
+            // soduko is solved
+        }
+
+        // backtrack
+        for (char number = '1'; number <= '9'; number++) {
+            if (isSafe(board, row, col, number)) {
+                board[row][col] = number;
+                if (solve(board)) {
+                    // found the answer
+                    return true;
+                } else {
+                    // backtrack
+                    board[row][col] = '.';
+                }
+            }
+        }
+        return false;
+    }
+
+
+
+
+    static boolean isSafe(char[][] board, int row, int col, int num) {
+        // check the row
+        for (int i = 0; i < board.length; i++) {
+            // check if the number is in the row
+            if (board[row][i] == num) {
+                return false;
+            }
+        }
+
+        // check the col
+        for (char[] nums : board) {
+            // check if the number is in the col
+            if (nums[col] == num) {
+                return false;
+            }
+        }
+
+        int sqrt = (int)(Math.sqrt(board.length));
+        int rowStart = row - row % sqrt;
+        int colStart = col - col % sqrt;
+
+        for (int r = rowStart; r < rowStart + sqrt; r++) {
+            for (int c = colStart; c < colStart + sqrt; c++) {
+                if (board[r][c] == num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
